a2494987536806f0c66d776fb2e5ff861e367b86
wildcat-man <wmmxsw@163.com>
2019-04-16 18:16:22 +0800

    首页

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,6 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
-import { AtButton,AtForm,AtInput,AtMessage,AtCard } from 'taro-ui'
+import { AtButton,AtForm,AtInput,AtMessage,AtCard,AtList,AtListItem } from 'taro-ui'
 import './index.scss'
 export default class Index extends Component {
 
@@ -33,9 +33,9 @@ export default class Index extends Component {
   }
   render () {
     return (
-      <View className='index'>
+      <View className='index' style={{backgroundColor:'#ECECEC'}}>
         {/*<AtButton onClick={this.onPath.bind(this)}>登录</AtButton>*/}
-        <View style={{display:'flex',flexWrap:'wrap',paddingBottom:'12px',backgroundColor:'#ECECEC'}}>
+        <View style={{display:'flex',flexWrap:'wrap',paddingBottom:'12px'}}>
           <View style={{width:'50%',marginTop:'12px'}}>
             <AtCard
               extra='额外信息'
@@ -74,7 +74,7 @@ export default class Index extends Component {
           </View>
         </View>
 
-        <View>
+        <View style={{backgroundColor:"#fff",paddingBottom:"12px"}}>
           <View style={{padding:"12px"}}>
             <Text>
               进行中的项目
@@ -102,8 +102,34 @@ export default class Index extends Component {
           </View>
         </View>
 
-        <View>
-
+        <View style={{marginTop:"12px",padding:'0 0 12px 0',backgroundColor:"#fff"}}>
+          <View style={{padding:'12px'}}>
+            <Text>
+              个人任务信息提醒
+            </Text>
+          </View>
+          <View>
+            <AtList>
+              <AtListItem
+                title='标题文字'
+                arrow='right'
+                thumb='https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png'
+              />
+              <AtListItem
+                title='标题文字'
+                note='描述信息'
+                arrow='right'
+                thumb='http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png'
+              />
+              <AtListItem
+                title='标题文字'
+                note='描述信息'
+                extraText='详细信息'
+                arrow='right'
+                thumb='http://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png'
+              />
+            </AtList>
+          </View>
         </View>
 
       </View>
